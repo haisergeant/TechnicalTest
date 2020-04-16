6fbb11aea7af07e4ef36612da402a40ad0741af4
Hai Le Thanh <haisergeant@gmail.com>
2020-04-17 09:15:50 +1000

    Initial Commit

---
+++ TechnicalTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TechnicalTest
+//
+//  Created by Hai Le Thanh on 4/17/20.
+//  Copyright © 2020 Hai Le Thanh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
